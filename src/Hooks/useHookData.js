13051f7f457b820ca97b63cc21caae73d20df723
yousufmiah <yousufweb99@gmail.com>
2022-05-03 17:07:20 +0600

    add something

--- src/Hooks/useHookData.js
+++ src/Hooks/useHookData.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const useHookData = () => {
   const [items, setItems] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:5000/items")
+    fetch("https://secret-inlet-51070.herokuapp.com/items")
       .then((res) => res.json())
       .then((data) => setItems(data));
   }, []);
